ff5f80baf282b1ea74922e63189c4e58e675b2f0
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-29 14:42:33 +0800

    add: create post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @programmer = Programmer.find(params[:programmer_id])
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    @programmer = Programmer.find(params[:post][:programmer_id])
+    redirect_to root_path
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content, :programmer_id)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_112152) do
+ActiveRecord::Schema.define(version: 2018_10_29_032307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2018_10_27_112152) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "programmer_id"
+    t.index ["programmer_id"], name: "index_posts_on_programmer_id"
   end
 
   create_table "programmers", force: :cascade do |t|
@@ -48,5 +50,6 @@ ActiveRecord::Schema.define(version: 2018_10_27_112152) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "programmers"
   add_foreign_key "programmers", "users"
 end

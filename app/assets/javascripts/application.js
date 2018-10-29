e4752bf3c33de0e0a513ece15774cc9a8b46e652
weiwenl <weiwenl.justin@gmail.com>
2018-10-29 10:29:39 +0800

    Fn for user's Profile created

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require_tree .
 
+//= require jquery_ujs
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :programmer
+  # before_create :build_programmer
+  # accepts_nested_attributes_for :programmer
 end

85949684510f84ed75d2c35e72242d24419883fc
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-30 15:27:17 +0800

    modify: latest updates on comments form and show view posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :programmers do
-    resources :posts do
-      resources :comments
-    end
+    resources :posts
+  end
+
+  resources :posts do
+    resources :comments
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

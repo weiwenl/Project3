e9149371c13898150cfff7172a8e17a4a43b523b
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-30 10:02:21 +0800

    add: column post-title with subsequent modified posts view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_032307) do
+ActiveRecord::Schema.define(version: 2018_10_30_015030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_10_29_032307) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "programmer_id"
+    t.string "post_title"
     t.index ["programmer_id"], name: "index_posts_on_programmer_id"
   end
 

5b1fb5f951f2fc49a7d23bf089f9b7dd643b6f17
weiwenl <weiwenl.justin@gmail.com>
2018-10-30 09:58:06 +0800

    Edit new form, mechanism for user index page, update p.table data type

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2018_10_27_112152) do
 
   create_table "programmers", force: :cascade do |t|
     t.string "name"
-    t.integer "level"
+    t.string "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
@@ -48,5 +48,6 @@ ActiveRecord::Schema.define(version: 2018_10_27_112152) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "programmers", "users"
+    add_foreign_key "posts", "programmers"
+    add_foreign_key "programmers", "users"
 end

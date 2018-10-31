a38ff661956a0f299d87e5a5fd32e924db868ce5
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-31 13:09:50 +0800

    changed: posts route and path

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,25 +8,22 @@ class PostsController < ApplicationController
   end
 
   def new
-    @programmer = Programmer.find(params[:programmer_id])
   end
 
   def edit
     @post = Post.find(params[:id])
-    @programmer = Programmer.find(params[:programmer_id].to_i)
   end
 
   def create
     @post = Post.new(post_params)
     @post.save
-    @programmer = Programmer.find(params[:programmer_id].to_i)
-    redirect_to root_path
+    redirect_to posts_path
   end
 
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to  programmer_posts_path
+    redirect_to posts_path
   end
 
   def destroy
@@ -38,7 +35,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:content, :programmer_id, :post_title)
+    params.require(:post).permit(:content, :post_title)
   end
 
 end

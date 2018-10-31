b002e9d76a3f3610ae8d00ce1be9caebed0be2f9
weiwenl <weiwenl.justin@gmail.com>
2018-10-31 14:14:00 +0800

    User can upload profile pic with cloudinary

--- app/controllers/programmers_controller.rb
+++ app/controllers/programmers_controller.rb
@@ -11,37 +11,51 @@ class ProgrammersController < ApplicationController
 
   end
   def create #create user: name & level
-    @programmer = Programmer.new(programmer_params)
-    #@programmers = Programmer.new(programmers_params)
-    @programmer.user_id = current_user.id
-    #@programmers.user_id = current_user.id
+
+      uploaded_file = params[:programmer][:photo_url].path
+      cloudnary_file = Cloudinary::Uploader.upload(uploaded_file)
+
+      params[:programmer][:photo_url] = cloudnary_file['public_id']
+      @programmer = Programmer.new(programmer_params)
+      @programmer.user_id = current_user.id
 
     if @programmer.save
       redirect_to root_path
     else
       render 'new'
     end
-    # @programmer = current_user.build_programmer(programmer_params)
-    # redirect to programmers_path
 
   end
   def show #this will show the user's profile
-    @programmers = Programmer.find_by(:user_id => current_user.id)
+    @programmers = Programmer.find_by(:user_id => current_user)
   end
   def edit #definitely allow users to edit their name and level
+    # @programmers = Programmer.find_by(:id => current_user)
+    # #if params[:programmers][:photo_url]
+    #   uploaded_file = params[:photo_url].path
+    #   cloudnary_file = Cloudinary::Uploader.upload(uploaded_file)
+    #
+    #   params[:photo_url] = cloudnary_file['public_id']
+    # else
+    #   params[:photo_url] = $result.data.image_original_url
+  #  end
 
   end
   def update #edit and update
-    # @profile.update(programmer_params)
-    # redirect_to(programmer_path(@programmer))
-    #redirect_to :show if current_user.update programmer_params
+    @programmers = Programmer.find(params[:id])
+    uploaded_file = params[:programmers][:photo_url].path
+    cloudnary_file = Cloudinary::Uploader.upload(uploaded_file)
+
+    params[:programmers][:photo_url] = cloudnary_file['public_id']
+
+    @programmers.update(programmer_params)
+    redirect_to root_path
   end
 
   private
 
   def programmer_params
-     #params.require(:user).permit(programmer_attributes: [:name])
-     params.require(:programmer).permit(:name, :level, :user_id)
+     params.require(:programmer).permit(:name, :level, :user_id, :photo_url)
   end
 
 end

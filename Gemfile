1f95075e76a90ac4561362296ad98a2096a9e6c1
weiwenl <weiwenl.justin@gmail.com>
2018-10-31 14:32:10 +0800

    user can upload pic on create

--- Gemfile
+++ Gemfile
@@ -44,6 +44,10 @@ gem 'devise'
 
 gem 'active_enum', '~> 1.0.0.pre'
 
+gem 'cloudinary'
+
+gem 'carrierwave'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_050620) do
+ActiveRecord::Schema.define(version: 2018_10_31_015751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_10_30_050620) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.text "photo_url"
     t.index ["user_id"], name: "index_programmers_on_user_id"
   end
 

2cb5ab2c0044797eab003477768df40648faaf4c
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-11-02 14:45:14 +0800

    add: programmer's name in post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def index
     @post = Post.all
     @programmer = Programmer.find(current_user.programmer.id)
+    @postFiltered = Post.where(programmer_id: current_user.programmer.id)
   end
 
   def show
